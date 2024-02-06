56e92d2aedae784571c9c342fcafa170c7e9258a
Nam Tran <qnam.ca@gmail.com>
2024-02-06 01:48:28 -0500

    Add topbar component

---
+++ react-app/src/components/topbar/Topbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faXTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faLocation, faLocationDot, faMapPin, faMarker, faMobilePhone, faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
+
+import "./topbar.css"
+import "../../styles/styles.css"
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+        <div className="topContainer">
+            <div className="hotelInfo">
+                <div className="hotelInfoItem">
+                    <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                    <p>3241 Elder Lane, Thunder Bay, P7E5P5, ON</p>
+                </div>
+                <div className="hotelInfoItem">
+                    <span><FontAwesomeIcon icon={faPhoneSquare}/></span>
+                    <p>+1 807 709 8998</p>
+                </div>
+                <div className="hotelInfoItem">
+                    <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                    <p>hello@westbestern.com</p>
+                </div>
+            </div>
+            <div className="socialMedia">
+                <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faFacebook} /></a>
+                <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faXTwitter} /></a>
+                <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faYoutube} /></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
--- react-app/src/pages/home/Home.jsx
+++ react-app/src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer';
+import Topbar from '../../components/topbar/Topbar';
 import "./home.css";
 
 const Home = () => {
   return (
     <div>
+        <Topbar/>
         <Navbar/>
         <Header/>
         <Footer/>
