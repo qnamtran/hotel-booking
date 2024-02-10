4edde1ef00db5827697fa29260abc107f6a215f3
Nam Tran <qnam.ca@gmail.com>
2024-02-10 10:47:27 -0500

    Add Contact Us modal to Topbar

--- react-app/src/components/topbar/Topbar.jsx
+++ react-app/src/components/topbar/Topbar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faXTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faLocation, faLocationDot, faMapPin, faMarker, faMobilePhone, faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 
 import "./topbar.css"
 import "../../styles/styles.css"
+
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const Topbar = () => {
   return (
@@ -24,7 +28,41 @@ const Topbar = () => {
                     <p>hello@westbestern.com</p>
                 </div>
             </div>
-            <a className='contact-btn'>Contact Us</a>
+            
+            
+            <Popup trigger=
+                {<a className='contact-btn'>Contact Us</a>} 
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div>
+                                <span className='close-btn' onClick=
+                                    {() => close()}>
+                                        <FontAwesomeIcon icon={faCircleXmark}/>
+                                </span>
+                            </div>
+                            <div className='content'>
+                                <div className="hotelInfoPopup">
+                                    <h2>Contact Us</h2>
+                                    <div className="hotelInfoItem">
+                                        <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                                        <p>3241 Elder Lane, Thunder Bay, P7E5P5, ON</p>
+                                    </div>
+                                    <div className="hotelInfoItem">
+                                        <span><FontAwesomeIcon icon={faPhoneSquare}/></span>
+                                        <p>+1 807-709-8998</p>
+                                    </div>
+                                    <div className="hotelInfoItem">
+                                        <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                                        <p>hello@westbestern.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
             <div className="socialMedia">
                 <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faFacebook} /></a>
                 <a href='#' className="socialMediaItem"><FontAwesomeIcon icon={faXTwitter} /></a>
