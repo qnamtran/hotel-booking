d03d59b82b65bd988c7b989d5b549f5c8c475611
Nam Tran <qnam.ca@gmail.com>
2024-02-21 17:54:33 -0500

    Refactor css code for toproom section

--- react-app/src/components/toprooms/Toproom.jsx
+++ react-app/src/components/toprooms/Toproom.jsx
@@ -11,7 +11,7 @@ const Toproom = () => {
         <div className="topRoomContainer">
             <h1 className='mainTitle'>Our Top Rated Rooms</h1>
             <div className="roomItem">
-                <div className="roomImage"><img src="https://a0.muscache.com/im/pictures/airflow/Hosting-1015487761074565473/original/b597e6c3-d7db-49c1-a317-fafca0d37478.jpg?im_w=1440" alt="" /></div>
+                <div className="roomCoverPhoto"><img src="https://a0.muscache.com/im/pictures/airflow/Hosting-1015487761074565473/original/b597e6c3-d7db-49c1-a317-fafca0d37478.jpg?im_w=1440" alt="" /></div>
                 <div className="roomContent">
                     <h2>2 Queen Beds Room</h2>
                     <p>Enjoy the fantastic views from this double queen suite with large seating area. Utilize the ergonomic chair and catch up with work at the desk. Surf the web with WiFi. Experience ultimate comfort in the memory foam mattress. </p>
@@ -106,7 +106,7 @@ const Toproom = () => {
                     </div>
                     <button className="primary-btn">Book Yours Now</button>
                 </div>
-                <div className="roomImage"><img src="https://a0.muscache.com/im/pictures/airflow/Hosting-1015487761074565473/original/e45a2b53-77eb-458c-a590-abb84781a89d.jpg?im_w=1440" alt="" /></div>
+                <div className="roomCoverPhoto"><img src="https://a0.muscache.com/im/pictures/airflow/Hosting-1015487761074565473/original/e45a2b53-77eb-458c-a590-abb84781a89d.jpg?im_w=1440" alt="" /></div>
             </div>
             <button className="secondary-btn rounded-btn animated-btn">View all of our rooms <span className='icon'><FontAwesomeIcon icon={faArrowRight} /></span></button>
         </div>
