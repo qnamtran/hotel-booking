e3263d3085eb79f39b0a2292bfa4a1d916c17592
mallo_CP390 <mallo@DELL5430>
2024-03-13 15:14:20 -0400

    attempt 2

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const roomBookingsRoute = require('./routes/roomBookings');
-const employeesRoute = require('./routes/employees');
-const cleaningDutiesRoute = require('./routes/cleaningDuties');
-const roomsRoute = require('./routes/rooms');
-const path = require('path');
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const roomBookingsRoute = require('./routes/roomBookings');
+// const employeesRoute = require('./routes/employees');
+// const cleaningDutiesRoute = require('./routes/cleaningDuties');
+// const roomsRoute = require('./routes/rooms');
+// const path = require('path');
+
 
 const app = express();
 
@@ -29,22 +30,39 @@ mongoose.connect('', {
 });
 */
 
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected.");
+});
 app.use(express.json());
 app.use(cors());
 
-// Route to serve adminPortal.html as index page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'AdminPortal', 'adminPortal.html'));
-});
+// // Route to serve adminPortal.html as index page
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'AdminPortal', 'adminPortal.html'));
+// });
 
 
-// Route to serve CRUD pages within AdminPortal folder
-app.get('/admin/:page', (req, res) => {
-  const page = req.params.page;
-  const fileName = `${page}AP.html`;
-  res.sendFile(path.join(__dirname, 'AdminPortal', fileName));
-});
+// // Route to serve CRUD pages within AdminPortal folder
+// app.get('/admin/:page', (req, res) => {
+//   const page = req.params.page;
+//   const fileName = `${page}AP.html`;
+//   res.sendFile(path.join(__dirname, 'AdminPortal', fileName));
+// });
 
 // API routes
 app.use('/api/room-bookings', roomBookingsRoute);
@@ -52,13 +70,15 @@ app.use('/api/employees', employeesRoute);
 app.use('/api/cleaning-duties', cleaningDutiesRoute);
 app.use('/api/rooms', roomsRoute);
 
-/* ports
-const PORT = 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-});
-*/
+// /* ports
+// const PORT = 8080;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on PORT ${PORT}`);
+// });
+// */
+
 app.listen(8800, () => {
   connect();
   console.log("Connected to backend.");
